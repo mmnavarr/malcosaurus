7f75ecac585f212fb0689637127cf7c6cd24b67a
Malcolm Navarro <malcolmcyber@gmail.com>
2020-10-13 21:36:03 -0500

    Added new cheatsheet section

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,7 +22,7 @@ module.exports = {
         {
           to: 'docs',
           position: 'left',
-          label: 'Docs',
+          label: 'Notes',
         },
         {
           to: '/',
--- sidebars.js
+++ sidebars.js
@@ -2,5 +2,6 @@ module.exports = {
   docs: {
     'Cache': ['caching', 'redis_vs_memcached'],
     'Data Stores': ['sql_vs_nosql', 'cap_theorem', 'acid_and_base', 'data_partitioning', ],
+    'Cheatsheets': ['npm_cheatsheet'],
   },
 };
