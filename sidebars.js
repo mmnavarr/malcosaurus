700fdceec09f3c4a8f4cf347a8de97cb1a4fb34f
Malcolm Navarro <malcolmcyber@gmail.com>
2020-12-14 21:55:04 -0600

    Added indexes doc to data store category

--- sidebars.js
+++ sidebars.js
@@ -3,7 +3,7 @@ module.exports = {
     'How To': ['install_curl'],
     'Object Oriented Programming': ['solid_principles'],
     'Cache': ['caching', 'redis_vs_memcached'],
-    'Data Stores': ['sql_vs_nosql', 'cap_theorem', 'acid_and_base', 'data_partitioning', ],
+    'Data Stores': ['sql_vs_nosql', 'cap_theorem', 'acid_and_base', 'data_partitioning', 'indexes'],
     'Web': ['session_vs_local_storage'],
     'Cheatsheets': ['git_cheatsheet', 'npm_cheatsheet', 'curl_cheatsheet'],
   },
