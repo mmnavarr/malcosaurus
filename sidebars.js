fcb7537c5e70e83273971efbcc85ef2b6d6dfc92
Malcolm Navarro <mnavarro@templuminc.com>
2020-12-28 15:31:48 -0600

    Added redis cheatsheet

--- sidebars.js
+++ sidebars.js
@@ -6,6 +6,6 @@ module.exports = {
     'Data Stores': ['sql_vs_nosql', 'cap_theorem', 'acid_and_base', 'data_partitioning', 'indexes'],
     'Web': ['session_vs_local_storage'],
     'Client Server Computing': [ 'load_balancing', 'proxies'],
-    'Cheatsheets': ['git_cheatsheet', 'npm_cheatsheet', 'curl_cheatsheet'],
+    'Cheatsheets': ['git_cheatsheet', 'npm_cheatsheet', 'curl_cheatsheet', 'redis_cheatsheet'],
   },
 };
